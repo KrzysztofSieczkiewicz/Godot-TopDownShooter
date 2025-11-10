3f2ad57534bec9356ebc7650acc9f5d3186ae549
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-11-10 23:17:21 +0100

    wip start implementing Transition rules

---
+++ PlayerMovement/StateMachine/Transitions/ILocomotionTransitionRule.gd
@@ -0,0 +1,5 @@
+class_name ILocomotionTransitionRule extends Resource
+
+func can_transition(parent: CharacterBody3D) -> bool:
+	push_error("can_transition for ITransitionRul")
+	return false
---
+++ PlayerMovement/StateMachine/Transitions/Locomotion/TransitionToRun.gd
@@ -0,0 +1,1 @@
+class_name LocomotionTransitionToRun extends ILocomotionTransitionRule
