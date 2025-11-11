d6883d988a3508baaefeccbcc969a1709eba98e3
kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-11-11 14:09:52 +0100

    locomotion sprint transition rules

---
+++ PlayerMovement/StateMachine/Transitions/Locomotion/Sprint/SprintToRun.gd
@@ -0,0 +1,6 @@
+class_name SprintToRunTransitionRule extends ILocomotionTransitionRule
+
+func can_transition(parent: CharacterBody3D, input: PlayerInput) -> bool:
+	var wants_to_run = input.locomotion_actions.has("run")
+	
+	return wants_to_run
