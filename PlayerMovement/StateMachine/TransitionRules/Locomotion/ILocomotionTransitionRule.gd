e63fff40b558ab6f7354eb2b55e320a47017d82c
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-11-30 22:45:48 +0100

    torso transition rules wip

--- PlayerMovement/StateMachine/TransitionRules/Locomotion/ILocomotionTransitionRule.gd
+++ PlayerMovement/StateMachine/TransitionRules/Locomotion/ILocomotionTransitionRule.gd
@@ -1,5 +1,5 @@
 class_name ILocomotionTransitionRule extends Resource
 
 func can_transition(parent: CharacterBody3D, input: PlayerInput) -> bool:
-	push_error("can_transition for ITransitionRule should be implemented")
+	push_error("can_transition for ILocomotionTransitionRule should be implemented")
 	return false
---
+++ PlayerMovement/StateMachine/TransitionRules/Torso/ITorsoTransitionRule.gd
@@ -0,0 +1,5 @@
+class_name ITorsoTransitionRule extends Resource
+
+func can_transition(parent: CharacterBody3D, input: PlayerInput) -> bool:
+	push_error("can_transition for ITorsoTransitionRule should be implemented")
+	return false
