a16e1b90f7b27ba88c667b8235fd0bbb4510eeab
kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-12-03 15:30:46 +0100

    placeholders for root transition adn machines

---
+++ PlayerMovement/StateMachine/TransitionRules/Root/IRootTransitionRule.gd
@@ -0,0 +1,5 @@
+class_name IRootTransitionRule extends Resource
+
+func can_transition(parent: CharacterBody3D, input: PlayerInput) -> bool:
+	push_error("can_transition for IRootTransitionRule should be implemented")
+	return false
