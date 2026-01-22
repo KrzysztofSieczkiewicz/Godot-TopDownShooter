f8dc1b303e3bca13383eeebad2bb23c9ac19159b
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2026-01-22 23:04:56 +0100

    handling mesh rotation when movement directions changes

--- Player/Model/AnimationManager/AnimationManager.gd
+++ Player/Model/AnimationManager/AnimationManager.gd
@@ -1,13 +1,6 @@
 extends Node
 
-var CURRENT_LOCOMOTION_MOVESETS: Dictionary[HumanStates.LOCOMOTION_GEAR, ILocomotionSet]
-var CURRENT_ANIMATIONS: Dictionary[float, StringName]
-
-func match_animation(velocity: Vector3):
-	pass
-
-func _get_matching_animation(velocity: Vector3):
-	pass
+@onready var locomotion_animation_player = $PlayerAnimationManager/LocomotionAnimator
 
 func update_locomotion(current_moveset: ILocomotionSet, 
 		velocity: float, 
@@ -20,7 +13,47 @@ func update_locomotion(current_moveset: ILocomotionSet,
 	
 	var motion_data = current_moveset.get_motion_data(movement_angle)
 	
-	## TODO: implement those
-	#_apply_speed_scaling(world_velocity.length(), motion.speed)
-	#_apply_rotation_offset(move_angle, motion.angle, delta)
-	#_play_synced(motion.name)
+	_update_speed_scaling(character_velocity.length(), motion_data.speed)
+	#_play_synced(motion_data.name)
+
+func _update_speed_scaling(char_speed: float, anim_move_speed: float):
+	if anim_move_speed <= 0.001: 
+		locomotion_animation_player.speed_scale = 1.0
+		return
+	
+	var target_scale = char_speed / anim_move_speed
+	
+	locomotion_animation_player.speed_scale = move_toward(
+		locomotion_animation_player.speed_scale, 
+		target_scale, 
+		0.05
+	)
+
+func calculate_mesh_rotation(
+	movement_angle: float,  # angle between movement and looking directions
+	animation_angle: float, # angle from LocomotionSet
+	current_rot: float,     # current mesh rotation
+	delta: float) -> float:
+	var angle_diff_deg = wrapf(movement_angle - animation_angle, -180, 180)
+	var target_rad = deg_to_rad(angle_diff_deg)
+	
+	return lerp_angle(
+		current_rot, 
+		target_rad, 
+		15.0 * delta # the "flexibility" value; higher is more responsive
+	)
+
+### TODO: go through this - maybe find a more flexible approach so animations doesn't need to match in length and stride
+### TODO: also consider how to add blending
+func _play_synced(anim_name: String):
+	if locomotion_animation_player.current_animation == anim_name:
+		return
+	
+	# Save the current 'play head' position (e.g., 0.4 seconds into the step)
+	var current_pos = locomotion_animation_player.current_animation_position
+
+	locomotion_animation_player.play(anim_name)
+	
+	# rewind the new animation to the exact same spot so the feet can theoretically match
+	# This assumes your walk/run/strafe clips are all the same length and start on the same foot
+	locomotion_animation_player.seek(current_pos)
